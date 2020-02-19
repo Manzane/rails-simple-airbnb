a6b50a8b469b01730c8c1bb1d29a73b99c25e6fd
marie blanchet <marie.blanchet.pro@gmail.com>
2020-02-19 15:29:39 +0100

    Flat BnB code ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+    name:         'Light & Spacious Garden Flat London',
+    address:      '7 Boundary St, London E2 7JE',
+    description:  'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night:  60,
+    number_of_guests: 3
+  },
+  {
+    name:         'Imperial Palace',
+    address:      'Hue',
+    description:  'the perfect palace for all your friend',
+    price_per_night:  1000,
+    number_of_guests: 20
+  },
+  {
+    name:         'Cage à poules',
+    address:      'Paris 3ième',
+    description:  'A lovely little flat of 12m²',
+    price_per_night:  800,
+    number_of_guests: 1
+  },
+  {
+    name:         'Space Station',
+    address:      'Mars',
+    description:  'A beautiful capsule on Mars',
+    price_per_night:  300,
+    number_of_guests: 8
+  }
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
